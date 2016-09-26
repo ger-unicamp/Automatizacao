6eb98a3d6878a8d1c96ded54b7b12f0f01002b42
MrStrings <phstring95@hotmail.com>
2016-09-26 18:37:39 -0300

    Ajeitadinha no codigo

--- sistema.py
+++ sistema.py
@@ -4,16 +4,10 @@ import csv
 from time import sleep
 import pyupm_i2clcd as lcd
 
-myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62) #Inicializo o lcd na variavel myLcd
 
-myLcd.setCursor(0,0) #seta a posicao inicial do cursor
-
-myLcd.setColor(53, 39, 249) #cor de fundo em RGB
-
-myLcd.write('Digite sua senha')
-
+####################### FUNCOES #########################
 def criptografa(senha): #recebe a senha e retorna uma string da senha criptografada
-	
+
 	m = md5.new()
 
 	m.update(senha)
@@ -22,15 +16,17 @@ def criptografa(senha): #recebe a senha e retorna uma string da senha criptograf
 
 def autentifica(senha):
 
-	arq_senhas = csv.reader(open('senhas.csv', 'r')) #abre o arquivo csv com nomes e senhas
-
 	pessoas = []
 	senhas = []
+
+	arq_senhas = [] # Garante que arq_senhas exista
+	with open('senhas.csv', 'r') as raw_arq:
+		arq_senhas = csv.reader(raw_arq) #abre o arquivo csv com nomes e senhas
 
 	for linha in arq_senhas: #percorre o arquivo de pessoas e coloca os dados numa lista
 		pessoas.append(linha[0])
 		senhas.append(linha[1])
-	
+
 	senha_criptografada = criptografa(senha)
 
 	for i in range(len(senhas)):
@@ -42,7 +38,9 @@ def autentifica(senha):
 
 def adiciona_usuario():
 
-	arq_senhas = csv.reader(open('senhas.csv', 'r')) #abre o arquivo csv com nomes e senhas
+	arq_senhas = [] # Garante que arq_senhas exista
+	with open('senhas.csv', 'r') as raw_arq:
+		arq_senhas = csv.reader(open('senhas.csv', 'r')) #abre o arquivo csv com nomes e senhas
 
 	pessoas = []
 	senhas = []
@@ -61,18 +59,25 @@ def adiciona_usuario():
 	pessoas.append(nome)
 	senhas.append(senha_criptografada)
 
-	arq_senhas = open('senhas.csv', 'w')
 
-	for i in range(len(pessoas)):
-		arq_senhas.write(pessoas[i] + ',' + senhas[i] + '\n')
-	
+	with open('senhas.csv', 'r') as raw_arq:
+		for i in range(len(pessoas)):
+			raw_arq.write(pessoas[i] + ',' + senhas[i] + '\n')
+
+####################### MAIN BEHAVIOUR ################################
+
+
+myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62) #Inicializo o lcd na variavel myLcd
+
+myLcd.setCursor(0,0) #seta a posicao inicial do cursor
+
+myLcd.setColor(53, 39, 249) #cor de fundo em RGB
+
+myLcd.write('Digite sua senha')
 
 adiciona_usuario()
 
-print "Escreva a senha ja cadastrada"
-senha = raw_input()
-autentifica(senha)
-
-	
-
-
+while (true):
+	print "Escreva a senha ja cadastrada"
+	senha = raw_input()
+	autentifica(senha)
